8d49fa1048861bd307c6a6dc8ac804b0089cdc0f
Didi Chen <50339450+ddchen05@users.noreply.github.com>
2022-05-11 00:06:45 -0500

    Add files via upload

---
+++ Phase 3/lib/menu.php
@@ -0,0 +1,24 @@
+
+			<div id="header">
+                <div class="logo"><img src="img/Data_warehouse_logo.png" style="opacity:0.9;background-color:#FFFFFF;" border="0" alt="" title="Data Warehouse"/></div>
+			</div>
+			
+		<div class="nav_bar">
+				<ul> 
+		    <li><a>Maintaining</a></li>
+                    <li><a style="margin-left:2em" href="manager.php" <?php if($current_filename=='manager.php') echo "class='active'"; ?>>Manager</a></li>                       
+		    <li><a style="margin-left:2em" href="city.php" <?php if($current_filename=='city.php') echo "class='active'"; ?>>City</a></li>  		 
+                    <li><a style="margin-left:2em" href="holiday.php" <?php if($current_filename=='holiday.php') echo "class='active'"; ?>>Holiday</a></li>  
+                    <li><a href="report1.php" <?php if($current_filename=='report1.php') echo "class='active'"; ?>>Manufacturer's Product Report</a></li>  
+                    <li><a href="report2.php" <?php if($current_filename=='report2.php') echo "class='active'"; ?>>Category Report</a></li>  
+                    <li><a href="report3.php" <?php if($current_filename=='report3.php') echo "class='active'"; ?>>Actual versus Predicted Revenue</a></li>  
+                    <li><a href="report4.php" <?php if($current_filename=='report4.php') echo "class='active'"; ?>>Store Revenue by Year by State</a></li>  
+                    <li><a href="report5.php" <?php if($current_filename=='report5.php') echo "class='active'"; ?>>Air Conditioners on Groundhog Day</a></li>
+		    <li><a href="report6.php" <?php if($current_filename=='report6.php') echo "class='active'"; ?>>State with Highest Sell Volume</a></li>
+		    <li><a href="report7.php" <?php if($current_filename=='report7.php') echo "class='active'"; ?>>Revenue by Population</a></li>
+
+ <li><a style="margin-left:2em" href="DashBoard.php" <span class='glyphicon glyphicon-log-out'></span>Return DashBoard</a></li>              
+
+</ul>
+
+			</div>
